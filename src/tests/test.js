ea360f63e7e574dfe89f526c682df0ecc0fde23a
Keanu Hie <keanu.hie@strg.at>
2019-08-02 13:16:42 +0200

    Fixed wrong test

--- src/tests/test.js
+++ src/tests/test.js
@@ -2,7 +2,7 @@ const request = require('supertest')
 const app = require('../app.js')
 
 describe('GET /', () => {
-  it('responds with Hello world', (done) => {
-    request(app).get('/').expect('Hello world', done)
+  it('responds with Hey world', (done) => {
+    request(app).get('/').expect('Hey world', done)
   })
 })
